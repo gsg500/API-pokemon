bf13f62a74cf501e36778b08cdbe2e227080df8e
Guilherme Santos Gomes <guilherme0500@hotmail.com>
2021-12-13 00:12:20 -0300

    API JSON

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const regras = require("./pokemon/regras");
+const search = require("./pokemon/search");
+
+const PORT = 4000;
+const app = express();
+
+// -- Define your route listeners here! --
+app.use(express.json());
+app.use("/pokemon", regras);
+app.use("/search", search);
+app.listen(PORT, () => { console.log(`Server rodando na porta ${PORT}`); });
+
